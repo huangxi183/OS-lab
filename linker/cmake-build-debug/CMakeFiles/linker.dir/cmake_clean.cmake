f383a020d88311c9619216da6a40187f8cb0cc72
huangxi183 <huangxi183@gmail.com>
2017-03-01 15:47:55 -0500

    lab1-linker

---
+++ linker/cmake-build-debug/CMakeFiles/linker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linker.dir/main.cpp.o"
+  "linker.pdb"
+  "linker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
